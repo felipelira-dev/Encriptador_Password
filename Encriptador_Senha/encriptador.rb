049feac326debfbbb0d6cdc52a8c3b51d0e7e3e2
Felipe Pereira <163548758+felipelira-dev@users.noreply.github.com>
2026-04-01 01:44:42 -0300

    task_02 - complete e task_03 - em validação

--- Encriptador_Senha/encriptador.rb
+++ Encriptador_Senha/encriptador.rb
@@ -1,2 +1,11 @@
 puts "Me informe a senha que deseja encriptar: "
 senha = gets.chomp
+
+senha_caracteres = senha.chars.map do |char|
+ novo_valor = char.ord + 3
+    if novo_valor > 122
+        novo_valor -= 26
+    end
+end
+
+puts "A senha encriptada é: #{senha_caracteres}"
