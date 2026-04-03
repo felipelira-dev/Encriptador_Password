57946ca8ccaece711a78058cb5324e91407d7b28
Felipe Pereira <163548758+felipelira-dev@users.noreply.github.com>
2026-04-03 12:42:26 -0300

    Refatora a lógica de encriptação e desencriptação para corrigir a manipulação de caracteres

--- Encriptador_Senha/encriptador.rb
+++ Encriptador_Senha/encriptador.rb
@@ -7,29 +7,45 @@ opcao = gets.chomp
 if opcao == "1"
     puts "Me informe a senha que deseja encriptar: "
     senha = gets.chomp
+    # Encripta a senha usando uma cifra de César, onde cada letra é substituída pela letra que está 3 posições à frente no alfabeto.
+    senha_caracteres = senha.chars.map do |char|
+        if char.match?(/[a-zA-Z]/)
+            novo_valor = char.ord + 3
+            # Verifica se o caractere é uma letra minúscula e se o novo valor ultrapassa 'z' (122 em ASCII)
+            if char.match?(/[a-z]/) && novo_valor > 122 
+                novo_valor -= 26
+            # Verifica se o caractere é uma letra maiúscula e se o novo valor ultrapassa 'Z' (90 em ASCII)
+            elsif char.match?(/[A-Z]/) && novo_valor > 90 
+                novo_valor -= 26
+            end
+            novo_valor.chr # Converte o valor numérico de volta para um caractere
+        else
+            char # Se o caractere não for uma letra, mantém o mesmo caractere (como espaços ou pontuação)
+        end
+    end
 end
 if opcao == "2"
     puts "Me informe a senha que deseja desencriptar: "
     senha = gets.chomp
+        # Desencripta a senha usando uma cifra de César, onde cada letra é substituída pela letra que está 3 posições para trás no alfabeto.
+    senha_caracteres = senha.chars.map do |char|
+        if char.match?(/[a-zA-Z]/)
+            novo_valor = char.ord - 3
+
+            # Verifica se o caractere é uma letra minúscula e se o novo valor ultrapassa 'z' (122 em ASCII)
+            if char.match?(/[a-z]/) && novo_valor < 97
+                novo_valor += 26
+            # Verifica se o caractere é uma letra maiúscula e se o novo valor ultrapassa 'Z' (90 em ASCII)
+            elsif char.match?(/[A-Z]/) && novo_valor < 65
+                novo_valor += 26
+            end
+            novo_valor.chr # Converte o valor numérico de volta para um caractere
+        else
+            char # Se o caractere não for uma letra, mantém o mesmo caractere (como espaços ou pontuação)
+        end
+    end
 end
 
-# Encripta a senha usando uma cifra de César, onde cada letra é substituída pela letra que está 3 posições à frente no alfabeto.
-senha_caracteres = senha.chars.map do |char|
- novo_valor = char.ord + 3
-    if novo_valor > 122
-        novo_valor -= 26
-    end
-    novo_valor.chr
-end
-
-# Desencripta a senha usando a mesma cifra de César, onde cada letra é substituída pela letra que está 3 posições atrás no alfabeto.
-senha_caracteres = senha.chars.map do |char|
-    novo_valor = char.ord - 3
-    if novo_valor < 97
-        novo_valor += 26
-    end
-    novo_valor.chr
-end
 
 # Exibe a senha encriptada
 puts "A senha encriptada é: #{senha_caracteres.join}"
